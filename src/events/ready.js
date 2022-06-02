a546099dcc25e36d7f0f67ad6c824760beb37c1a
MostlyGhostlyGaming <70042305+AlbinoFoxGithub@users.noreply.github.com>
2022-06-02 02:18:07 -0500

    update 1.0.2

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'ready',
     once: true,
-    async execute() {
-        console.log('ready');
+    async execute(client) {
+        console.log('Ready!');
+        client.user.setPresence({ activities: [{ name: 'Template By AlbinoFox#6901' }], status: 'idle' }) // "name:" is the text that appears in your activity. "status:" is your bots status (online, idle, dnd, invisible)
     },
 };
