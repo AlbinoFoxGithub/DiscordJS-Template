21cec3f780fc27145ccb1d11a26006716b7394f2
MostlyGhostlyGaming <70042305+AlbinoFoxGithub@users.noreply.github.com>
2022-06-03 02:46:10 -0500

    fixed ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log('Ready!');
-        client.user.setPresence({ activities: [{ name: 'Template By AlbinoFox#6901' }], status: 'idle' }) // "name:" is the text that appears in your activity. "status:" is your bots status (online, idle, dnd, invisible)
+        client.user.setPresence({ activities: [{ name: 'Template By AlbinoFox#6901' }], status: 'idle' }); // "name:" is the text that appears in your activity. "status:" is your bots status (online, idle, dnd, invisible)
     },
 };
